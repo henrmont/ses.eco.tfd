41b085d22e791a8bf33e288c2f796d14b109edd0
Jorge Monteiro <dev.henrmont@gmail.com>
2026-09-24 14:14:30 -0400

    (fix) resolver

--- resolvers/tfd-user-resolver.ts
+++ resolvers/tfd-user-resolver.ts
@@ -3,7 +3,7 @@ import { ResolveFn } from '@angular/router';
 import { catchError, of } from 'rxjs';
 import { UserService } from '../services/user.service'; // Serviço dentro do módulo TFD
 
-export const userResolver: ResolveFn<any> = (route, state) => {
+export const tfdUserResolver: ResolveFn<any> = (route, state) => {
   const userService = inject(UserService);
 
   // Busca os dados atualizados do perfil profissional / lotações do usuário no módulo TFD
